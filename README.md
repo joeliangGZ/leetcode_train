e99de014369ed1431d8b46058c7a23e72249e23d
joeliang <>
2024-11-06 17:54:13 +0800

    chore:

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ LeetCode 刷题.
 5. 链表删除重复元素: 链表严格升序，比较下一个和下下个 》deleteDuplicates
 6. 两个数组排序：逆向双指针 》merge，要求原来的两个数组非递减，且第一个数据尾部有空间
 7. 二叉树的三种遍历顺序 》BST.md
+8. 二叉树（相同的树）
+   9. 深度优先搜索 》 isSameTreeDFS 最优解，递归、栈，回溯，后进先出 
+   10. 广度优先搜索 》 isSameTreeBFS 最短路径，队列，先进先出
+11. 二叉树，镜像(相同的树) 》 深度优先搜索、广度优先搜索 isSymmetricDFS、isSymmetricBFS
+12. 
---
+++ src/main/java/org/example/simple/isSameTreeDFS.java
@@ -0,0 +1,52 @@
+package org.example.simple;
+
+public class isSameTreeDFS {
+
+    public static void main(String[] args) {
+        // Create an instance of the isSameTree class
+        isSameTreeDFS isSameTreeDFS = new isSameTreeDFS();
+        // Create two trees with the same structure and values
+        TreeNode p = isSameTreeDFS.new TreeNode(1, isSameTreeDFS.new TreeNode(2), isSameTreeDFS.new TreeNode(3));
+        TreeNode q = isSameTreeDFS.new TreeNode(1, isSameTreeDFS.new TreeNode(2), isSameTreeDFS.new TreeNode(3));
+        // Print the result of the isSameTree method
+        System.out.println(isSameTreeDFS.isSameTree(p, q));
+    }
+
+    // Method to check if two trees are the same
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // If both trees are null, they are the same
+        if (p == null && q == null) {
+            return true;
+        }
+        // If one of the trees is null, they are not the same
+        if (p == null || q == null) {
+            return false;
+        }
+        // If the values of the nodes are not the same, they are not the same
+        if (p.val != q.val) {
+            return false;
+        }
+        // Recursively check the left and right subtrees
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
