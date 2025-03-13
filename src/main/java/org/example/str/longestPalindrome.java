003f17da04e62a7d913b7cbc7793c2c27bbf185e
joeliang <>
2025-03-13 18:13:13 +0800

    chore: submit longestPalindromeInDP

--- src/main/java/org/example/str/longestPalindrome.java
+++ src/main/java/org/example/str/longestPalindrome.java
@@ -32,4 +32,43 @@ public class longestPalindrome {
 
         return right - left - 1;
     }
+
+    public String longestPalindromeInDP(String s) {
+        int n = s.length();
+        if (n < 2) {
+            return s;
+        }
+
+        boolean[][] dp = new boolean[n][n];
+        int maxLength = 1;
+        int start = 0;
+
+        // 初始化：所有长度为 1 的子串都是回文子串
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        // 填充 dp 数组
+        for (int j = 1; j < n; j++) {
+            for (int i = 0; i < j; i++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i <= 2) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                } else {
+                    dp[i][j] = false;
+                }
+
+                // 更新最长回文子串的起始位置和长度
+                if (dp[i][j] && j - i + 1 > maxLength) {
+                    maxLength = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLength);
+    }
 }
