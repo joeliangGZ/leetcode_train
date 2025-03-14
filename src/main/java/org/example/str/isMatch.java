2cd29ce7272e3669ca80ef9880dba054bd68ebe5
joeliang <>
2025-03-14 18:57:14 +0800

    chore: add comment on 10 (dp)

--- src/main/java/org/example/str/isMatch.java
+++ src/main/java/org/example/str/isMatch.java
@@ -32,15 +32,21 @@ public class isMatch {
             }
         }
 
+        /**
+         * 状态转移：
+         *
+         * 如果 p[j-1] 是 *，则有两种情况：
+         *      * 代表零个前面的字符：dp[i][j] = dp[i][j-2]
+         *      * 代表一个或多个前面的字符：dp[i][j] = dp[i-1][j]，且 s[i-1] 与 p[j-2] 匹配（即 s[i-1] == p[j-2] 或 p[j-2] == '.'）。
+         * 如果 p[j-1] 不是 *，
+         *      则 dp[i][j] = dp[i-1][j-1] 且 s[i-1] 与 p[j-1] 匹配（即 s[i-1] == p[j-1] 或 p[j-1] == '.'）。
+         */
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (p.charAt(j - 1) == '*') {
-                    dp[i][j] = dp[i][j - 2];
-                    if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
-                        dp[i][j] = dp[i][j] || dp[i - 1][j];
-                    }
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.'));
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.');
                 }
             }
         }
