22759fb8deadb60d1dd522d2236eb1689b6a719b
joeliang <>
2025-03-14 15:42:08 +0800

    chore: 5(dp solve)/11

--- src/main/java/org/example/z_practise/longestPalindrome.java
+++ src/main/java/org/example/z_practise/longestPalindrome.java
@@ -29,4 +29,40 @@ public class longestPalindrome {
 
         return j - i + 1;
     }
+
+    public String longestPalindromeInDP(String s) {
+
+        int n = s.length();
+        if (n < 2) {
+            return s;
+        }
+
+        int start = 0, maxLength = 0;
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < j; i++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i <= 2) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                } else {
+                    dp[i][j] = false;
+                }
+
+                if (dp[i][j]  && j - i + 1 > maxLength) {
+                    maxLength = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLength);
+    }
 }
