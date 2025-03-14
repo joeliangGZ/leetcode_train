0eee32411820e2d503987dd0e7084acac4ecaf33
joeliang <>
2025-03-14 18:07:20 +0800

    chore: add comment on 10 (dp)

--- src/main/java/org/example/str/isMatch.java
+++ src/main/java/org/example/str/isMatch.java
@@ -2,6 +2,21 @@ package org.example.str;
 
 /**
  * 10. 正则表达式匹配 Hard
+ *
+ * dp[i][j] 为 true 表示 s 的前 i 个字符（即 s[0...i-1]）和 p 的前 j 个字符（即 p[0...j-1]）是匹配的。
+ * dp[i][j] 为 false 表示 s 的前 i 个字符和 p 的前 j 个字符不匹配。
+ *
+ * 假设 s = "aab"，p = "c*a*b"，我们可以构建如下的 dp 表格：
+ *          c	*	a	*	b
+ *      0	1	2	3	4	5
+ * 0	T	F	T	F	T	F
+ * 1	F	F	T	T	T	F
+ * 2	F	F	F	T	T	F
+ * 3	F	F	F	F	T	T
+ *
+ * dp[0][0] 为 true，表示空字符串和空模式是匹配的。
+ * dp[1][2] 为 true，表示 s 的前1个字符（"a"）和 p 的前2个字符（"c*"）是匹配的，因为 * 可以匹配零个 c。
+ *
  */
 public class isMatch {
 
